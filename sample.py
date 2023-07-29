ac6a858cc73a7c4598e840c9f71ba40ad6ee2544
Jeff Sontag <jmoso13@gmail.com>
2023-07-29 14:56:36 -0700

    comma

--- sample.py
+++ sample.py
@@ -234,7 +234,7 @@ def main():
   parser.add_argument('--dd-noise-style', help='How the random noise for generating in Dance Diffusion progresses: random, constant, region, walk', default='random', type=str)
   parser.add_argument('--noise-step-size', help='How far to wander around init noise, should be between 0-1, if set to 0 will act like constant noise, if set to 1 will act like random noise', default=0.05, type=float)
   parser.add_argument('--dd-noise-step-size', help='How far to wander around init DD noise, should be between 0-1, if set to 0 will act like constant noise, if set to 1 will act like random noise', default=0.05, type=float)
-  parser.add_argument('--token-multiplier' help='Multiplier for base_tokens', default=1, type=int)
+  parser.add_argument('--token-multiplier', help='Multiplier for base_tokens', default=1, type=int)
   args = parser.parse_args()
 
 
